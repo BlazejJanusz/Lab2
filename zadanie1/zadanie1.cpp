916556030eea4440f126c21883c1ae1b5bd19bae
BlazejJanusz <149466097+BlazejJanusz@users.noreply.github.com>
2023-10-31 10:04:38 +0100

    Add files via upload

---
+++ zadanie1/zadanie1.cpp
@@ -0,0 +1,74 @@
+﻿
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+
+int main()
+{ 
+	/*
+	int x;
+	int y = 3;
+	cout << "Podaj liczbe";
+
+	do
+		
+	if (x % 2 != 0 && x >= 3)
+		cin >> x;
+	{
+		y = 0;
+	}
+	else 
+	{
+		if (y == 1)
+			cout << "liczba prob sie skonczyla";
+		else
+			y = y - 1;
+		cout << "podaj inna liczba";
+	}
+			
+	while (y != 0);
+	*/
+	int IloscWierszy;
+		cout << "podaj ilosc wierszy";
+		cin >> IloscWierszy; 
+	
+		int IloscKolumn = IloscWierszy, Liczbaspacji = IloscWierszy - 1;
+		char Znak;
+
+		for (int wiersz = 0; wiersz < IloscWierszy; wiersz++)
+		{
+			for (int kolumna = 0; kolumna <= IloscKolumn; kolumna++)
+			{
+				Znak = (kolumna <= Liczbaspacji) ? ' ' : '#';
+				cout << Znak;
+			}
+			cout << endl;
+			IloscKolumn++;
+			Liczbaspacji--;
+
+		}
+		IloscKolumn = IloscWierszy, Liczbaspacji = 0;
+		for (int wiersz = 1; wiersz < IloscWierszy; wiersz++)
+		{
+			for (int kolumna = 0; kolumna <= IloscKolumn; kolumna++)
+			{
+				Znak = (kolumna <= Liczbaspacji) ? ' ' : '#';
+				cout << Znak;
+			}
+			cout << endl;
+			IloscKolumn++;
+			Liczbaspacji++;
+
+		}
+
+
+
+
+}
+
+
+
+
